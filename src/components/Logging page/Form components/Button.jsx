834ed524b6ba076003fb6d97c20b303b81411415
InvisibleHorse <pieceofcake.okay@gmail.com>
2023-06-14 23:53:47 +0200

    Fake auth, axios, dispatch

--- src/components/Logging page/Form components/Button.jsx
+++ src/components/Logging page/Form components/Button.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import s from './Form.module.css';
 
-export default function Button() {
-    return <button type="submit" className={`${s.button} ${s.formText} btn`}>Log me in</button>;
+export default function Button({ onClick, text, disabled }) {
+    return (
+        <button
+            disabled={disabled}
+            type="submit"
+            className={`${s.button} ${s.formText} btn`}
+            onClick={onClick}
+        >{text}
+        </button>
+    );
 }
+Button.propTypes = {
+    onClick: PropTypes.func,
+    text: PropTypes.string,
+    disabled: PropTypes.bool,
+};
